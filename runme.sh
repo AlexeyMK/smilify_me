7b0a7a4efd650f75c27c166a04b80ddf9153834a
Alexey Komissarouk <alexeymk@gmail.com>
2012-01-16 01:04:49 -0500

    updated runme.sh to take from master branch on github

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-#download imagesnap and the python script
-curl http://dl.dropbox.com/u/2001/ADAAHackNY/imagesnap > imagesnap
-curl http://dl.dropbox.com/u/2001/ADAAHackNY/thanks_for_helping.py > thanks_for_helping.py 
+# download the necessary script and utility 
+curl https://github.com/AlexeyMK/smilify_me/raw/master/imagesnap > imagesnap
+curl https://raw.github.com/AlexeyMK/smilify_me/master/thanks_for_helping.py > thanks_for_helping.py 
 
 #make sure imagesnap is executable
 chmod +x imagesnap
